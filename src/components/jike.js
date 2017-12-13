40c5be59e4764680664ec9bc82ac4b35bd852554
Arthur Wang <arthraim@gmail.com>
2017-12-13 17:57:11 +0800

    draw with react-konva

--- src/components/jike.js
+++ src/components/jike.js
@@ -1,35 +1,38 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Stage, Layer, Rect, Text } from 'react-konva'
 
 class JikeComponent extends React.Component {
   render() {
     const { text } = this.props
     return (
-      <div style={this._style().div}>
-        <span style={this._style().span}>{ text }</span>
-      </div>
+      <Stage width={512} height={512}>
+        <Layer>
+          <Rect
+            x={0}
+            y={0}
+            width={512}
+            height={512}
+            fill={'#FDDF13'}
+          />
+          <Text
+            x={0}
+            y={0}
+            width={512}
+            height={512}
+            offsetY={-(512-400)/2}
+            text="J"
+            fill="#FFFFFF"
+            align="center"
+            fontSize={400}
+            shadowColor='#5EC1FA'
+            shadowOffset={{x: 20, y: 15}}
+            shadowBlur={0}
+            shadowOpacity={1.0}
+          />
+        </Layer>
+      </Stage>
     )
-  }
-
-  _style() {
-    return {
-      div: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 512,
-        height: 512,
-        background: '#fddf13',
-        background: 'linear-gradient(135deg, #fddf13 0%,#fddf13 30%,#fdc413 100%)',
-      },
-      span: {
-        lineHeight: '512px',
-        fontFamily: 'Avenir',
-        fontSize: 400,
-        color: '#FFFFFF',
-        textShadow: '20px 15px #5EC1FA'
-      },
-    }
   }
 }
 
